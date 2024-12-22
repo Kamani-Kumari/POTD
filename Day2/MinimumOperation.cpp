6db9b57ae8fc5b8daf743dc4b3dc1656d37b2eae
ShopUniverse <kamanikumarikamanikumari1@gmail.com>
2024-12-22 11:22:45 +0530

    day2

---
+++ Day2/MinimumOperation.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        int count=0;
+         for(int i =0;i<n;i++){
+        if(nums[i]%3==1){
+                count++;
+        }
+        else if(nums[i]%3==2){
+               count++;
+        }
+         }
+        return count;
+    }
+};
---
+++ Day2/SingleNumber.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       /* sort(nums.begin(),nums.end());
+        int n = nums.size();
+        for(int i = 1; i<n;i+=2){
+        if(nums[i-1]!=nums[i])
+            return nums[i-1];
+        
+        }
+        */
+        int res = 0;
+        for(int i =0; i<nums.size(); i++){
+              res = res ^nums[i];
+        }
+        return res;
+    }
+};
+int main(){
+      Solution s;
+      vector<int> v={1,2,3,1};
+      cout<< s.singleNumber(v);
+      return 0;
+}
